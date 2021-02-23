1b10a4babe535101cc09e1271247d9351020c57c
Noan <noan.crespel@epita.fr>
2021-02-23 22:43:33 +0100

    1st real commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Squashed
-S2 Projet
+Projet de S2 par des étudiants d'EPITA.
+
+Nous avons eu comme projet pour le S2 de créer un jeu vidéo de type stratégie au tour par tour. Développé sur Unity, il mettra en scene des combats de fourmi et de termites.
+
---
+++ Squashed/Assets/Scripts/Move_Script.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Move_Script : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Camera camera;
+    public Grid grid;
+    public bool clicked;
+    public SpriteRenderer spriteRenderer;
+    public Sprite movesprite;
+    public Vector3 mousepos;
+    
+    
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (clicked && Input.GetMouseButtonDown(0))
+        {
+            //Récupère la position de la souris par rapport à la camera
+            mousepos = camera.ScreenToWorldPoint(Input.mousePosition);
+            //Permet de changer la position en position par rapport aux tiles
+            mousepos = grid.WorldToCell(mousepos);
+            //Change la position sur la case ou le joueur a cliqué
+            transform.position = new Vector3(mousepos.x+0.5f,mousepos.y+0.5f,0);
+        }
+    }
+
+    //Se lance quand on clique sur le collider de l'objet
+    void OnMouseDown()
+    {
+        clicked = true;
+        //Change le sprite actuel en movesprite
+        //spriteRenderer.sprite = movesprite;
+        
+    }
+
+    private void OnMouseExit()
+    {
+        
+    }
+}
